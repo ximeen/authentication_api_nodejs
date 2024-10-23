c8157230aa2d4f968361a0f5573632a3ea6ddeaf
Gabriel Ximenes <swg-ximenes@iMac-Pro-de-Gabriel.local>
2024-10-23 20:41:24 -0300

    :package: feat create user tested

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,5 +4,5 @@ import { CreateUserController } from './user/createUserController'
 export async function appRoutes(app: FastifyInstance) {
   const authenticateUserController = new CreateUserController()
 
-  app.get('/register', authenticateUserController.handle)
+  app.post('/register', authenticateUserController.handle)
 }
--- src/controllers/user/createUserController.ts
+++ src/controllers/user/createUserController.ts
@@ -5,14 +5,13 @@ import { UserSchema } from '../../services/user/createUserServiceDTO'
 
 export class CreateUserController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
-    const { name, password, username } = UserSchema.parse(request.body)
-
     const createUserService = new CreateUserService()
-    const newUser = createUserService.execute({
+    const { name, password, username } = UserSchema.parse(request.body)
+    const newUser = await createUserService.execute({
       name,
       password,
       username,
     })
-    return { newUser }
+    return newUser
   }
 }
